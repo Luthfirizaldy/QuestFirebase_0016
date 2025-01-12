bc07c48c9ce2e3a64e760d9f6c341619210f00a1
Nurachmad Luthfi Rizaldy <nurachmadluthfi83@gmail.com>
2025-01-13 00:18:23 +0700

    Navigasi Halaman dan Controller disetiap halaman

--- app/src/main/java/com/example/firebase/navigation/Controller.kt
+++ app/src/main/java/com/example/firebase/navigation/Controller.kt
@@ -1,2 +1,28 @@
 package com.example.firebase.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.firebase.ui.view.HomeScreen
+
+@Composable
+fun MainControllerPage(
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route
+    ) {
+        composable(
+            route = DestinasiHome.route
+        ) {
+            HomeScreen(
+                onDetailClick = {},
+                navigateToItemEntry = {},
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/firebase/navigation/Destinasi.kt
+++ app/src/main/java/com/example/firebase/navigation/Destinasi.kt
@@ -1,2 +1,9 @@
 package com.example.firebase.navigation
 
+interface HalamanController {
+    val route : String
+}
+
+object DestinasiHome : HalamanController {
+    override val route = "home"
+}
