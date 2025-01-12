90de056025b748576526eb3f9998b5a14658771a
Nurachmad Luthfi Rizaldy <nurachmadluthfi83@gmail.com>
2025-01-13 00:31:34 +0700

    MahasiswaApp

---
+++ app/src/main/java/com/example/firebase/MahasiswaApp.kt
@@ -0,0 +1,24 @@
+package com.firebase
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.firebase.navigation.MainControllerPage
+
+@Composable
+fun MahasiswaApp() {
+    Scaffold (
+        modifier = Modifier
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            MainControllerPage()
+        }
+    }
+}
